373b4278f646f50fd9d0000257ad8f95609980b8
AntoineDrsl <antoine.durussel@gmail.com>
2021-12-10 16:14:11 +0100

    :sparkles: FEAT monaco-editor

--- src/views/games/MultiErrors.vue
+++ src/views/games/MultiErrors.vue
@@ -9,12 +9,22 @@
                     <div class="progression">
                         <span>Progression: 1/10</span>
                         <div class="progress mt-1">
-                            <div class="progress-bar bg-success" role="progressbar" style="width: 10%"></div>
+                            <div class="progress-bar" role="progressbar" style="width: 10%"></div>
                         </div>
                     </div>
+                    <div id="editor-1"></div>
+                    <div class="button">TESTER</div>
                 </div>
                 <div class="opponent">
                     <h3 class="geminis subtitle">Ton adversaire: Player 2</h3>
+                    <div class="progression">
+                        <span>Progression de ton adversaire: 1/10</span>
+                        <div class="progress mt-1">
+                            <div class="progress-bar" role="progressbar" style="width: 10%"></div>
+                        </div>
+                    </div>
+                    <div id="editor-2"></div>
+                    <img src="@/assets/images/games/astronaut.png" alt="Astronaut" class="astronaut">
                 </div>
             </div>
         </template>
@@ -36,13 +46,13 @@
                 <div class="link-container active">
                     <a href="#" class="geminis">Space games</a>
                     <div class="img-container">
-                        <img src="@/assets/images/games/params-icon.png" alt="Space games">
+                        <img src="@/assets/images/games/code-icon.png" alt="Space games">
                     </div>
                 </div>
                 <div class="link-container">
                     <a href="#" class="geminis">Paramètres</a>
                     <div class="img-container">
-                        <img src="@/assets/images/games/code-icon.png" alt="Paramètres">
+                        <img src="@/assets/images/games/params-icon.png" alt="Paramètres">
                     </div>
                 </div>
             </div>
@@ -64,12 +74,48 @@
 <script>
     import GameLayout from '@/layouts/GameLayout'
     import SpaceButton from '@/components/SpaceButton'
+    import * as monaco from 'monaco-editor'
+    import $ from 'jquery'
 
     export default {
         name: 'MultiErrors',
         components: {
             GameLayout,
             SpaceButton
+        },
+        data() {
+            return {
+                language: 'php',
+                editorGamer: null,
+                output: null
+            }
+        },
+        methods: {
+            executeCode() {
+                $.ajax({
+                    url: 'http://localhost:3000/editor',
+                    method: 'POST',
+                    data: {
+                        language: this.language,
+                        code: monaco.editor.getModels()[0].getValue()
+                    },
+                    success: (res) => {
+                        this.output = res;
+                    }
+                })
+            }
+        },
+        mounted() {
+            this.editorGamer = monaco.editor.create(document.getElementById("editor-1"), {
+                value: "<?php \n\nfunction hello() {\n\techo 'Hello world !';\n} \nhello();",
+                language: "php",
+                theme: 'vs-dark'
+            });
+            this.editorOpponent = monaco.editor.create(document.getElementById("editor-2"), {
+                value: "<?php \n\nfunction hello() {\n\techo 'Hello world !';\n} \nhello();",
+                language: "php",
+                theme: 'vs-dark'
+            });
         }
     }
 </script>
@@ -97,6 +143,36 @@
     }
     .explanation {
         font-size: 0.8rem;
+    }
+    .progression {
+        margin-bottom: 20px;
+        font-size: 0.8rem;
+    }
+    #editor-1 {
+        height: 320px;
+    }
+    #editor-2 {
+        height: 180px;
+    }
+    .progress-bar {
+        background-color: #44F5BA;
+    }
+    .button {
+        text-align: center;
+        background-color: #fff;
+        border-radius: 30px;
+        font-weight: 700;
+        padding: 4px 0;
+        box-shadow: 0px 0px 50px 5px rgba(48,181,255,0.8);
+        transition: 0.25s;
+        cursor: pointer;
+        color: #090B31;
+        width: 20%;
+        margin-top: 20px;
+    }
+    .astronaut {
+        width: 110%;
+        margin-top: 15px;
     }
 
     /* Sidebar */
