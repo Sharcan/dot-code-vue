36b6d2af1764d23e98957b7d2f3bbddebee281c7
AntoineDrsl <antoine.durussel@gmail.com>
2022-03-18 12:24:57 +0100

    :construction: UPDATE socker-auth

--- src/middlewares/socket-auth.js
+++ src/middlewares/socket-auth.js
@@ -1,18 +1,21 @@
 import axios from "axios";
 
-export default function socketAuth(socket) {
+export default async function socketAuth(socket) {
     const userId = localStorage.getItem('user');
     if(userId) {
-        axios.patch(process.env.VUE_APP_API_URL + 'user/' + userId, {
-            socket_id: socket.client.id
-        });
-    } else {
-        axios.post(process.env.VUE_APP_API_URL + 'user', {
-            socket_id: socket.client.id,
-            is_guest: true
-        }).then(res => {
-            const newId = res.data.raw[0].id;
-            localStorage.setItem('user', newId);
-        });
+        const user = await axios.get(process.env.VUE_APP_API_URL + 'user/' + userId).then(res => res.data)
+        if(user) {
+            axios.patch(process.env.VUE_APP_API_URL + 'user/' + userId, {
+                socket_id: socket.client.id
+            });
+            return;
+        }
     }
+    axios.post(process.env.VUE_APP_API_URL + 'user', {
+        socket_id: socket.client.id,
+        is_guest: true
+    }).then(res => {
+        const newId = res.data.raw[0].id;
+        localStorage.setItem('user', newId);
+    });
 }   
